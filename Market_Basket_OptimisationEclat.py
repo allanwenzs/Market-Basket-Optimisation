690e6f11187d63069c4689e9be3ac8080afe601f
Allan Wenzslaus <allanwenzslaus@live.com>
2024-06-02 11:12:06 +0100

    Update Market_Basket_OptimisationEclat.py
    Debugs updated

--- Market_Basket_OptimisationEclat.py
+++ Market_Basket_OptimisationEclat.py
@@ -1,4 +1,3 @@
-
 #Importing the required libraries
 import pandas as pd
 import numpy as np
@@ -7,7 +6,7 @@ import matplotlib.pyplot as plt
 #Reading and processing dataset from csv file
 df = pd.read_csv('market_Basket_OptimisationEclat.csv', header=None)
 transactions=[]
-for i in range(0, 70):
+for i in range(0, 7501):
 	#pass the values
 	transactions.append([str(df.values[i,j])for j in range(0,20)])
 	transactions[:2]
@@ -21,8 +20,14 @@ results = list(rules)
 print(results)
 ### Putting the results well organised into a Pandas DataFrame and display the results
 def inspect(results):
-	lhs = [tuple(result[2][0][0])[0] for result in results]
-	rhs = [tuple(result[2][0][1])[0] for result in results]
-	support = [result[1] for result in results]
-	return list(zip(lhs, rhs, support))
-resultsInDataFrame= pd.DataFrame(inspect(results), columns=['Product 1', 'Product 2', 'Support'])
+    lhs         = [tuple(result[2][0][0])[0] for result in results]
+    rhs         = [tuple(result[2][0][1])[0] for result in results]
+    supports    = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts       = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+resultsinDataFrame
+print('Data in organised format')
+### Displaying the results sorted by descending lifts
+print(resultsinDataFrame.nlargest(n = 10, columns = 'Lift'))
